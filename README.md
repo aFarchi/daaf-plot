f49b618a91a8a6249869432cb5655d631767b188
Alban Farchi <alban.farchi@enpc.fr>
2025-09-30 12:15:03 +0200

    included all from afplot

---
+++ README.md
@@ -0,0 +1,3 @@
+# daaf-plot
+
+A python toolbox to make interactive plot in python notebooks.
--- src/daaf_plot/transforms/linlogp.py
+++ src/daaf_plot/transforms/linlogp.py
@@ -4,34 +4,14 @@ from numpy import ma
 
 
 class LinLogPressureTransform(mtransforms.Transform):
-    """Forward transformation for the hybrid linear-logarithmic axis scale.
-
-    Attributes:
-        threshold: lower bound for pressure.
-    """
-
     input_dims = 1
     output_dims = 1
 
     def __init__(self, threshold):
-        """Initialise the transformation.
-
-        Args:
-            threshold: lower bound for pressure.
-        """
         mtransforms.Transform.__init__(self)
         self.threshold = threshold
 
     def transform_non_affine(self, pressure):
-        """Apply the scale.
-
-        Args:
-            pressure: array of pressure values to transform.
-
-        Returns:
-            Array of transformed pressure. Values below the
-            threshold are masked.
-        """
         masked = ma.masked_where(pressure < self.threshold, pressure)
         if masked.mask.any():
             return ma.where(
@@ -46,38 +26,18 @@ class LinLogPressureTransform(mtransforms.Transform):
         )
 
     def inverted(self):
-        """Get the inverse transformation."""
         return InvertedLinLogPressureTransform(self.threshold)
 
 
 class InvertedLinLogPressureTransform(mtransforms.Transform):
-    """Inverse transformation for the hybrid linear-logarithmic axis scale.
-
-    Attributes:
-        threshold: lower bound for pressure.
-    """
-
     input_dims = 1
     output_dims = 1
 
     def __init__(self, threshold):
-        """Initialise the transformation.
-
-        Args:
-            threshold: lower bound for pressure.
-        """
         mtransforms.Transform.__init__(self)
         self.threshold = threshold
 
     def transform_non_affine(self, y_coordinate):
-        """Apply the scale.
-
-        Args:
-            y_coordinate: array of y-coordinates to inverse transform.
-
-        Returns:
-            Array of pressure, computed by inverting the y-coordinates.
-        """
         return np.where(
             y_coordinate >= 2,
             100 + (y_coordinate - 2) * (1000 - 100) / 3,
@@ -85,5 +45,4 @@ class InvertedLinLogPressureTransform(mtransforms.Transform):
         )
 
     def inverted(self):
-        """Get the inverse transformation."""
         return LinLogPressureTransform(self.threshold)
